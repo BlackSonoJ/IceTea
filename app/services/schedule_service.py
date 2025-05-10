40816fdbc937c136b268d6ed52da873926a16d4b
Владимир Лигатюк <vligatyuk@gmail.com>
2025-05-10 21:36:18 +0200

    polished services to level, when it almost looks like it was written from nothing

--- app/services/schedule_service.py
+++ app/services/schedule_service.py
@@ -1,54 +1,62 @@
 from datetime import datetime, date, timedelta, time
+from typing import Annotated
 
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.params import Depends
 
-from app.schemas import ScheduleGetSchema
-from app.config import period  # начинал как временный
-from app.repositories.schedule_repositoty import ScheduleRepository
-from app.utils.round_minutes import round_minutes
+from app.schemas.schedule import ScheduleGet, NextTaking
+from app.core.config import period
+from app.integrations.schedule_repo import ScheduleRepository
+from app.utils.count_next_takings import get_schedule_next_takings
+from app.core.dependencies import get_schedule_repo
 
 
 class ScheduleService:
     """Место для вашей рекламы"""
 
-    def __init__(self, session: AsyncSession):
-        self.repository = ScheduleRepository(session=session)
+    def __init__(
+        self,
+        repository: Annotated[ScheduleRepository, Depends(get_schedule_repo)],
+    ):
+        self._repository = repository
 
-    async def get_single_schedule(self, user_id: int, schedule_id: int):
-        schedule = await self.repository.get_single_schedule(
+    async def get_schedule(self, user_id: int, schedule_id: int) -> ScheduleGet | None:
+        schedule = await self._repository.get_schedule(
             user_id=user_id,
             schedule_id=schedule_id,
         )
-        return ScheduleGetSchema.model_validate(schedule) if schedule else None
+        return ScheduleGet.model_validate(schedule) if schedule else None
 
-    async def create_single_schedule(self, schedule: dict):
-        return {
-            "schedule_id": await self.repository.create_single_schedule(
-                schedule=schedule
-            )
-        }
+    async def create_schedule(self, schedule: dict) -> int:
+        return await self._repository.create_schedule(schedule=schedule)
 
-    async def get_schedules_list(self, user_id: int):
-        schedules_list = await self.repository.get_schedules_list(user_id=user_id)
-        return [
-            ScheduleGetSchema.model_validate(schedule) for schedule in schedules_list
-        ]
+    async def get_schedules_list(self, user_id: int) -> list[ScheduleGet]:
+        schedules_list = await self._repository.get_schedules_list(user_id=user_id)
+        return [ScheduleGet.model_validate(schedule) for schedule in schedules_list]
 
-    async def get_next_takings(self, user_id: int):
-        schedules_list = await self.repository.get_schedules_list_for_today(
+    async def get_next_takings(
+        self,
+        user_id: int,
+        period_start: datetime = None,
+        notification_send_start=None,
+    ) -> list[dict]:
+        schedules_list = await self._repository.get_schedules_list_for_today(
             user_id=user_id
         )
-        result = set()
-        period_start = datetime.now()
-        period_end = period_start + period
-        notification_send_start = datetime.combine(date.today(), time(hour=8))
+        result = []
         for schedule in schedules_list:
-            interval = timedelta(hours=14) / schedule.periodicity
-            times = [
-                round_minutes(notification_send_start + i * interval)
-                for i in range(schedule.periodicity)
-            ]
-            for calculated_time in times:
-                if period_start <= calculated_time <= period_end:
-                    result.add(schedule.medicine_name)
-        return list(result)
+            result.extend(
+                get_schedule_next_takings(
+                    period=period,
+                    period_start=period_start if period_start else datetime.now(),
+                    notification_send_start=(
+                        notification_send_start
+                        if notification_send_start
+                        else datetime.combine(
+                            date=date.today(),
+                            time=time(hour=8),
+                        )
+                    ),
+                    schedule=schedule,
+                ),
+            )
+        return result
