00b2ceeb21761babb93a1d8037a58e8d27197825
Владимир Лигатюк <vligatyuk@gmail.com>
2025-05-10 21:44:33 +0200

    moved generated logic of grpc inside app

--- app/generated/protos/schedule/schedule_pb2_grpc.py
+++ app/generated/protos/schedule/schedule_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import generated.protos.schedule.schedule_pb2 as schedule__pb2
+from app.generated.protos.schedule import schedule_pb2 as schedule__pb2
 
 GRPC_GENERATED_VERSION = "1.71.0"
 GRPC_VERSION = grpc.__version__
